8573ed6d016024719d4f684fd38140026a16a0bf
pruk17 <chaiyapruk6494@gmail.com>
2025-07-28 01:11:06 +0700

    move to another folder

--- ReceiverTest.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-ser = serial.Serial('/dev/ttyUSB0', 115200)  # adjust port and baud rate
-
-while True:
-    line = ser.readline().decode('utf-8').strip()  # read one line from serial
-    print("Received:", line)
